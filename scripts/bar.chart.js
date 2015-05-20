31ad0618723ad785506d3d4ca251803248a903a4
Evelyn Luu <eyluu@uci.edu>
2015-05-20 16:27:11 -0700

    yyyy

--- scripts/bar.chart.js
+++ scripts/bar.chart.js
@@ -43,17 +43,17 @@ d3.tsv("HCI_Top10.tsv", function(error, data) {
 
   var translateRotateLegend = d3.svg.transform().translate(-5,0).rotate(-90);
 	
-  svg.append("text")
+  svg.append("g")
+      .attr("class", "y axis")
+	  .attr("transform", "translate(-10,0)")
+      .call(yAxis)
+    .append("text")
 	  .attr("transform", translateRotateLegend)
       .attr("y", 6)
       .attr("dy", ".71em")
       .style("text-anchor", "end")
       .text("Score");
-    .append("g")
-      .attr("class", "y axis")
-	  .attr("transform", "translate(-10,0)")
-      .call(yAxis)
-	  
+
   svg.selectAll(".bar")
       .data(data)
     .enter().append("rect")
