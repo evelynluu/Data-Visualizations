076ea1fc351a4ef6ece643d06a9f8e4695bdb1f5
Evelyn Luu <eyluu@uci.edu>
2015-05-20 16:55:38 -0700

    x test 2

--- scripts/bar.chart.js
+++ scripts/bar.chart.js
@@ -38,7 +38,7 @@ d3.tsv("HCI_Top10.tsv", function(error, data) {
 
   svg.append("g")
       .attr("class", "x axis")
-      .attr("transform", "translate(0," + height + 100 + ")")
+      .attr("transform", "translate(0," + height + 25 + ")")
       .call(xAxis);
 	
   svg.append("g")
