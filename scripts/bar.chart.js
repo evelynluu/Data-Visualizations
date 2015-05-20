e6a4316d4d028c7bf567d88910b41c7222b8447c
Evelyn Luu <eyluu@uci.edu>
2015-05-20 16:40:54 -0700

    not sure why revert isnt working

--- scripts/bar.chart.js
+++ scripts/bar.chart.js
@@ -2,9 +2,7 @@ var margin = {top: 50, right: 20, bottom: 30, left: 40},
     width = 960 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
 	
-var transformLegend = d3.svg.transform()
-	.translate(-10, 0)
-	.rotate(-90);
+
 
 var x = d3.scale.ordinal()
     .rangeRoundBands([0, width], .1);
