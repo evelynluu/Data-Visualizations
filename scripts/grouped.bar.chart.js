3373bdcd36aad5e87829828f9d65976c4fe9ce54
Evelyn Luu <eyluu@uci.edu>
2015-05-21 03:07:03 -0700

    re-did functions; changed csv to tsv

--- scripts/grouped.bar.chart.js
+++ scripts/grouped.bar.chart.js
@@ -1,33 +1,25 @@
 /* Sample Code Source: http://bl.ocks.org/mbostock/3887051 */
 
-
-var x0 = d3.scale.ordinal()
-    .rangeRoundBands([0, width], .1);
-
+/*Variables*/
 var x1 = d3.scale.ordinal();
-
-var y = d3.scale.linear()
-    .range([height, 0]);
 
 var color = d3.scale.ordinal()
     .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00", "#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00", "#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00", "#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00", "#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00", "#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
 
-var xAxis = d3.svg.axis()
-    .scale(x0)
-    .orient("bottom");
-
-var yAxis = d3.svg.axis()
-    .scale(y)
-    .orient("left")
-    .tickFormat(d3.format(".2s"));
-
-var svg = d3.select("div#grouped-bar-HCI").append("svg")
+/* Calling functions for creating graphs */
+createGroupedBarGraph("div#grouped-bar-HCI", "HCI_Top10.tsv");
+createGroupedBarGraph("div#grouped-bar-SE", "SE_Top10.tsv");
+createGroupedBarGraph("div#grouped-bar-CSCW", "CSCW_Top10.tsv");
+	
+/*Functions*/
+function createGroupedBarGraph(barID, dataset) {
+	var svg = d3.select(barID).append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
   .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-d3.csv("HCI_Top10.csv", function(error, data) {
+d3.tsv(dataset, function(error, data) {
   var facultyRank = d3.keys(data[0]).filter(function(key) { return key !== "school" && 
 																								   key !== "overall_rank" &&
 																								   key !== "overall_h_index_score" &&
@@ -41,8 +33,8 @@ d3.csv("HCI_Top10.csv", function(error, data) {
     d.rankings = facultyRank.map(function(name) { return {name: name, value: +d[name]}; });
   });
 
-  x0.domain(data.map(function(d) { return d.school; }));
-  x1.domain(facultyRank).rangeRoundBands([0, x0.rangeBand()]);
+  x.domain(data.map(function(d) { return d.school; }));
+  x1.domain(facultyRank).rangeRoundBands([0, x.rangeBand()]);
   y.domain([0, d3.max(data, function(d) { return d3.max(d.rankings, function(d) { return d.value; }); })]);
 
   svg.append("g")
@@ -64,7 +56,7 @@ d3.csv("HCI_Top10.csv", function(error, data) {
       .data(data)
     .enter().append("g")
       .attr("class", "g")
-      .attr("transform", function(d) { return "translate(" + x0(d.school) + ",0)"; });
+      .attr("transform", function(d) { return "translate(" + x(d.school) + ",0)"; });
 
   school.selectAll("rect")
       .data(function(d) { return d.rankings; })
@@ -74,24 +66,5 @@ d3.csv("HCI_Top10.csv", function(error, data) {
       .attr("y", function(d) { return y(d.value); })
       .attr("height", function(d) { return height - y(d.value); })
       .style("fill", function(d) { return color(d.name); });
-
-  /*var legend = svg.selectAll(".legend")
-      .data(facultyRank.slice().reverse())
-    .enter().append("g")
-      .attr("class", "legend")
-      .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
-
-  legend.append("rect")
-      .attr("x", width - 18)
-      .attr("width", 18)
-      .attr("height", 18)
-      .style("fill", color);
-
-  legend.append("text")
-      .attr("x", width - 24)
-      .attr("y", 9)
-      .attr("dy", ".35em")
-      .style("text-anchor", "end")
-      .text(function(d) { return d; });*/
-
-});
+   });
+}
